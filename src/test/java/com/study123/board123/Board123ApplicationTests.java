935d16721ebe6d34599fae89e78cab2a76cd61ca
Kim Jae Bin <jaebindana@gmail.com>
2023-10-10 13:13:05 +0900

    123

---
+++ src/test/java/com/study123/board123/Board123ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study123.board123;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Board123ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
